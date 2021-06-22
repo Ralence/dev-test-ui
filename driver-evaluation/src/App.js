d0ec5a62ecb5b32439c2d90367fb819487629b70
Ralence <ranko.ostojic@gmail.com>
2021-06-22 16:02:29 +0200

    feat: add PageTitle component

--- driver-evaluation/src/App.js
+++ driver-evaluation/src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/header/Header";
+import PageTitle from "./components/page-title/PageTitle";
 import styles from "./App.module.css";
 
 function App() {
   return (
     <div className={styles.App}>
       <Header />
+      <PageTitle />
     </div>
   );
 }
---
+++ driver-evaluation/src/components/page-title/PageTitle.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./pageTitle.module.css";
+
+const PageTitle = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Driver evaluation</h1>
+    </div>
+  );
+};
+
+export default PageTitle;
